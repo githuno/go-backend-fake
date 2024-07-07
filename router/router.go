8db0f444e7b00f63129295d6731f889b8a65335c
githuno <clas.warehouse@gmail.com>
2024-07-07 18:05:31 +0900

    CORS設定

--- router/router.go
+++ router/router.go
@@ -1,17 +1,35 @@
 package router
 
 import (
-    "github.com/gin-gonic/gin"
-    api "backend-fake/handlers/api" // 正しくインポートされているか確認
+	api "backend-fake/handlers/api" // 正しくインポートされているか確認
+
+	"github.com/gin-gonic/gin"
 )
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func SetupRouter() *gin.Engine {
-    router := gin.Default()
+	router := gin.Default()
+	router.Use(CORSMiddleware()) // CORSミドルウェアの適用
 
-    // JSONファイル操作用のルーティング
-    router.GET("/json/:filename", api.GetJsonFile)
-    router.PUT("/json/:filename", api.UpdateJsonFile)
-    router.POST("/json/:filename", api.CreateJsonFile)
+	// JSONファイル操作用のルーティング
+	router.GET("/json/:filename", api.GetJsonFile)
+	router.PUT("/json/:filename", api.UpdateJsonFile)
+	router.POST("/json/:filename", api.CreateJsonFile)
 
-    return router
+	return router
 }
